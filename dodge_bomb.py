77b1cea1e35a4e9bb18958e9b698ae834a3d1b09
nakata <c0b2310742@edu.teu.ac.jp>
2024-11-26 14:03:06 +0900

    練1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,7 +3,14 @@ import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 1100, 650
+DELTA = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -11,9 +18,9 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
-    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
+    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
-    kk_rct.center = 900, 400
+    kk_rct.center = 300, 200
     clock = pg.time.Clock()
     tmr = 0
     while True:
